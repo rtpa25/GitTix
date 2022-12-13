91441a8e140f54d2b4314f6c7acf451bcec3700d
Ronit Panda <pandaronit25@gmail.com>
2022-12-13 16:54:27 +0530

    feat(auth): updated connected mongo msg

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -11,7 +11,7 @@ const bootstrap = async () => {
     }
     try {
         await mongoose.connect(process.env.MONGO_URI, {});
-        console.log('Connected to MongoDB');
+        console.log('connected to MongoDB');
     } catch (error) {
         console.error(error);
     }
