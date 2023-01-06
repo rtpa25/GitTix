613dd3c7ca34bd12a3c919f73bb2087c71c35910
Ronit Panda <pandaronit25@gmail.com>
2023-01-06 19:30:10 +0530

    feat: testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -16,7 +16,7 @@ const bootstrap = async () => {
         console.error(error);
     }
     app.listen(3000, () => {
-        console.log('Server is running on port 3000!');
+        console.log('Server is running on port 3000!!!!');
     });
 };
 
